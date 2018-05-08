2eb55865542d22fd0651c6012e44ef9845f03cd9
Jurgen Beliën <jurgen@voorhoede.nl>
2018-05-08 18:56:26 +0200

    addd segmented links to viewer

--- src/views/Viewer.vue
+++ src/views/Viewer.vue
@@ -1,18 +1,22 @@
 <template>
   <div class="viewer">
     <liwo-map />
+    <segmented-links
+    />
   </div>
 </template>
 
 <script>
-import LiwoMap from '../components/LiwoMap.vue'
+import LiwoMap from '@/components/LiwoMap.vue'
+import SegmentedLinks from '@/components/SegmentedLinks'
 
 export default {
   components: {
-    LiwoMap
+    LiwoMap,
+    SegmentedLinks
   }
 }
 </script>
 <style>
 
-</style>å
+</style>
