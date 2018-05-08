faa74a32cb2cbf9b5b6dcc04e32c20bf24235b60
Jurgen Beliën <jurgen@voorhoede.nl>
2018-05-08 19:46:37 +0200

    add load by layer id

--- src/lib/load-layersets.js
+++ src/lib/load-layersets.js
@@ -1,8 +1,22 @@
-const body = JSON.stringify({ username: '', password: '', mode: '' })
+const apiBase = 'https://basisinformatie-overstromingen.nl/liwo.ws'
 const headers = { 'Accept': 'application/json', 'Content-Type': 'application/json' }
 
-export default async function loadLayersets () {
-  return fetch('https://basisinformatie-overstromingen.nl/liwo.ws/Authentication.asmx/Login', {
+export async function loadLayersetById (id) {
+  const body = JSON.stringify({ id })
+  return fetch(`${apiBase}/Maps.asmx/GetLayerSet`, {
+    method: 'POST',
+    mode: 'cors',
+    headers,
+    body
+  }).then(res => res.json())
+    .then(data => JSON.parse(data.d))
+    .then(data => data.layerset)
+    .catch(() => ([]))
+}
+
+export async function loadLayersets () {
+  const body = JSON.stringify({ username: '', password: '', mode: '' })
+  return fetch(`${apiBase}/Authentication.asmx/Login`, {
     method: 'POST',
     mode: 'cors',
     headers,
@@ -11,4 +25,9 @@ export default async function loadLayersets () {
     .then(data => JSON.parse(data.d))
     .then(data => data.layersets)
     .catch(() => ([]))
+}
+
+export default {
+  loadLayersetById,
+  loadLayersets
 }
--- src/views/Viewer.vue
+++ src/views/Viewer.vue
@@ -9,8 +9,16 @@
 <script>
 import LiwoMap from '@/components/LiwoMap.vue'
 import SegmentedLinks from '@/components/SegmentedLinks'
+import { loadLayersetById } from '@/lib/load-layersets'
 
 export default {
+  data () {
+    return { name: '', id: 0, layers: [] }
+  },
+  beforeCreate: async function () {
+    const { name, id, layers } = await loadLayersetById(this.$route.params.id)
+    Object.assign(this, { name, id, layers })
+  },
   components: {
     LiwoMap,
     SegmentedLinks
