c7fc9433b2f2959ddce69827f22adc58e2ab8203
Fedor Baart <fedor.baart@deltares.nl>
2018-05-14 10:54:48 +0200

    layout contact

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,14 +1,20 @@
 <template>
-  <div class="contact">
-    <p>
-      Heeft u inhoudelijke vragen over LIWO: dan kunt u hiervoor contact opnemen met het Watermanagementcentrum Nederland (WMCN). Het WMCN is bereikbaar op telefoonnummer <a href="tel:+31-320-298888">0320-298888</a> of per e-mail: <a href="mailto:wmcn@rws.nl">wmcn@rws.nl</a>.
-    </p>
-    <p>
-      Heeft u functionele vragen of aanvullende wensen met betrekking tot het gebruik van deze site: stuur dan een e-mail naar Rijkswaterstaat Water, Verkeer en Leefomgeving, afdeling Modellen en Applicaties: <a href="mailto:fb.hws@rws.nl">fb.hws@rws.nl</a>.
-    </p>
-    <p>
-      Is er sprake van een technische storing? Bel dan de landelijke informatielijn van Rijkswaterstaat (0800-8002). Meer achtergrondinformatie over LIWO vindt u op de website van de <a href="https://www.helpdeskwater.nl/onderwerpen/applicaties-modellen/applicaties-per/watermanagement/watermanagement/liwo/">Helpdesk Water</a>.
-    </p>
+  <div class="about site-main">
+    <div>
+      <section class="section">
+        <div class="contact">
+          <p>
+            Heeft u inhoudelijke vragen over LIWO: dan kunt u hiervoor contact opnemen met het Watermanagementcentrum Nederland (WMCN). Het WMCN is bereikbaar op telefoonnummer <a href="tel:+31-320-298888">0320-298888</a> of per e-mail: <a href="mailto:wmcn@rws.nl">wmcn@rws.nl</a>.
+          </p>
+          <p>
+            Heeft u functionele vragen of aanvullende wensen met betrekking tot het gebruik van deze site: stuur dan een e-mail naar Rijkswaterstaat Water, Verkeer en Leefomgeving, afdeling Modellen en Applicaties: <a href="mailto:fb.hws@rws.nl">fb.hws@rws.nl</a>.
+          </p>
+          <p>
+            Is er sprake van een technische storing? Bel dan de landelijke informatielijn van Rijkswaterstaat (0800-8002). Meer achtergrondinformatie over LIWO vindt u op de website van de <a href="https://www.helpdeskwater.nl/onderwerpen/applicaties-modellen/applicaties-per/watermanagement/watermanagement/liwo/">Helpdesk Water</a>.
+          </p>
+        </div>
+      </section>
+    </div>
   </div>
 </template>
 
@@ -17,5 +23,3 @@ export default {
   name: 'contact'
 }
 </script>
-<style>
-</style>
