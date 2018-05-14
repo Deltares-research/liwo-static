eb06f2c00e8d1862910a4bc1a2a186f90412380f
Fedor Baart <fedor.baart@deltares.nl>
2018-05-14 10:57:18 +0200

    fix some text

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -4,7 +4,7 @@
       <section class="section">
         <div class="contact">
           <p>
-            Heeft u inhoudelijke vragen over LIWO: dan kunt u hiervoor contact opnemen met het Watermanagementcentrum Nederland (WMCN). Het WMCN is bereikbaar op telefoonnummer <a href="tel:+31-320-298888">0320-298888</a> of per e-mail: <a href="mailto:wmcn@rws.nl">wmcn@rws.nl</a>.
+            Heeft u inhoudelijke vragen over LIWO: dan kunt u hiervoor contact opnemen met het Watermanagementcentrum Nederland (WMCN), bereikbaar op telefoonnummer <a href="tel:+31-320-298888">0320-298888</a> of per e-mail: <a href="mailto:liwo@rws.nl">liwo@rws.nl</a>.
           </p>
           <p>
             Heeft u functionele vragen of aanvullende wensen met betrekking tot het gebruik van deze site: stuur dan een e-mail naar Rijkswaterstaat Water, Verkeer en Leefomgeving, afdeling Modellen en Applicaties: <a href="mailto:fb.hws@rws.nl">fb.hws@rws.nl</a>.
