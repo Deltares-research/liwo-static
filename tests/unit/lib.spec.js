e9c7ac98f63445d30f36799439ed14dbfe8ad0aa
Fedor Baart <fedor.baart@deltares.nl>
2018-09-10 10:58:09 +0200

    remove hello world component

---
+++ tests/unit/lib.spec.js
@@ -0,0 +1,15 @@
+import { expect } from 'chai'
+import renderVue from '@/lib/render-vue'
+
+describe('the lib utlities contains a render vue', () => {
+  it('renders component', () => {
+    let element = renderVue(
+      {
+        render (createElement) { return createElement('div', this.$slots.default) }
+      },
+      {}
+    )
+    console.log(element)
+    expect(element).to.include('<div></div>')
+  })
+})
