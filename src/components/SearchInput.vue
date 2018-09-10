197144732f2f639e1f263f72a41db66c1a08d1ca
Stefan Kool <stefankool@MacBook-Pro-van-Stefan.local>
2018-09-10 12:13:29 +0200

    wip

---
+++ src/components/SearchInput.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <form>
+             <input type="text" name="search">
+        </form>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+    .search-input-control-container {
+        margin-right: 2.5rem;
+    }
+    .search-input-input-control {
+        background-color: #fff;
+    }
+</style>
