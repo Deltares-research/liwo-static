6e50d0e32744ecfd0a9e60c2e410666ce79aa7a4
Marko Malis <markomalis@hotmail.com>
2018-10-05 16:47:02 +0200

    fix(layercontrol): move the baselayers to the back when switching baselayers (#47)

--- src/lib/leaflet-utils/map-factory.js
+++ src/lib/leaflet-utils/map-factory.js
@@ -4,15 +4,17 @@ import '@/lib/leaflet-hack'
 import rdConfig from '@/lib/rijksdriehoek.config'
 import mapConfig from '@/map.config'
 
-// const MAPBOX_ACCES_TOKEN = 'pk.eyJ1Ijoic2lnZ3lmIiwiYSI6ImNqbHcwcHFjNjBsdGIza3F1dW5iZjhhY2EifQ.zZkkozZlSRtmB9VgpR9HiQ'
 const INITIAL_BASELAYER = mapConfig.tileLayers[0].title
 
 export default function (el, config) {
   const tileLayerOptions = baseLayerOptions(config)
   const baseLayers = createBaseLayers(tileLayerOptions)
-  const map = L.map(el, { ...config, crs: createCrs() })
+  const map = L.map(el, { ...config, crs: createCrs(), layers: [ baseLayers[INITIAL_BASELAYER] ] })
 
-  map.addLayer(baseLayers[INITIAL_BASELAYER])
+  // When we change the baselayer we want it to be in the back
+  map.on('baselayerchange', (e) => e.layer.bringToBack())
+
+  // map.addLayer(baseLayers[INITIAL_BASELAYER])
   map.setZoom(config.zoom || mapConfig.zoom)
 
   map.addControl(geoCoderControl(map))
@@ -49,8 +51,7 @@ function baseLayerOptions (config) {
     maxZoom: config.maxZoom || mapConfig.maxZoom,
     minZoom: config.minZoom || mapConfig.minZoom,
     tms: config.baseLayer.tms || mapConfig.baseLayer.tms,
-    continuousWorld: config.continuousWorld || mapConfig.continuousWorld,
-    zIndex: -1
+    continuousWorld: config.continuousWorld || mapConfig.continuousWorld
   }
 }
 
