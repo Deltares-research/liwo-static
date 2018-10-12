46360c171a2096c384fe8c057ea4d0288678b34e
Adriaanse <bart.adriaanse@gmail.com>
2018-10-12 11:37:56 +0200

    LIWO-112 : adjusted field names used for probability filter

--- src/lib/probability-filter.js
+++ src/lib/probability-filter.js
@@ -1,10 +1,10 @@
 export const probabilityConfig = [
   { title: 'alles weergeven', identifier: 'no_filter' },
   { title: 'grote kans (groter dan 1:30)', identifier: 'lt30' },
-  { title: 'middelgrote kans (1:30 tot 1:300)', identifier: 'from30to300' },
-  { title: 'kleine kans (1:300 tot 1:3000)', identifier: 'from300to3000' },
-  { title: 'zeer kleine kans (1:3000 tot 1:30.000)', identifier: 'from3000to30000' },
-  { title: 'extreem kleine kans (kleiner dan 1:30.000)', identifier: 'gt30000' }
+  { title: 'middelgrote kans (1:30 tot 1:300)', identifier: '30to300' },
+  { title: 'kleine kans (1:300 tot 1:3000)', identifier: '300to3000' },
+  { title: 'zeer kleine kans (1:3000 tot 1:30.000)', identifier: '3000to30k' },
+  { title: 'extreem kleine kans (kleiner dan 1:30.000)', identifier: 'gt30k' }
 ]
 
 export const probabilityTitles = probabilityConfig.map((entry) => entry.title)
