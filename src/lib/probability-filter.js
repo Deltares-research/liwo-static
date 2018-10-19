5de299d00f310f808aebdda0102b55be32d18103
Bart Adriaanse <bart.adriaanse@gmail.com>
2018-10-19 13:43:20 +0200

    Adjusted field names (again) for doorbraak probabilities (#52)

--- src/lib/probability-filter.js
+++ src/lib/probability-filter.js
@@ -1,9 +1,9 @@
 export const probabilityConfig = [
   { title: 'alles weergeven', identifier: 'no_filter' },
   { title: 'grote kans (groter dan 1:30)', identifier: 'lt30' },
-  { title: 'middelgrote kans (1:30 tot 1:300)', identifier: '30to300' },
-  { title: 'kleine kans (1:300 tot 1:3000)', identifier: '300to3000' },
-  { title: 'zeer kleine kans (1:3000 tot 1:30.000)', identifier: '3000to30k' },
+  { title: 'middelgrote kans (1:30 tot 1:300)', identifier: 'f30to300' },
+  { title: 'kleine kans (1:300 tot 1:3000)', identifier: 'f300to3000' },
+  { title: 'zeer kleine kans (1:3000 tot 1:30.000)', identifier: 'f3000to30k' },
   { title: 'extreem kleine kans (kleiner dan 1:30.000)', identifier: 'gt30k' }
 ]
 
