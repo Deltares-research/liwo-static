bf6e2dab07a49a2982cdc3ba716b4666275339bf
Sjoerd Beentjes <mail@sjoerdbeentjes.nl>
2019-03-11 16:04:13 +0100

    feat(leaflet-directive): add popup to show value of raster image

--- src/lib/leaflet-directive.js
+++ src/lib/leaflet-directive.js
@@ -1,5 +1,7 @@
 import layerFactory from './leaflet-utils/layer-factory'
 import mapFactory from './leaflet-utils/map-factory'
+import L from './leaflet-utils/leaf'
+import getFeatureInfo from './get-feature-info'
 
 let map
 let currentLayers = []
@@ -23,6 +25,37 @@ export default {
         map.addLayer(layer)
         return layer
       })
+
+    map.on('click', e => {
+      if (newCurrentLayers.length) {
+        const layer = newCurrentLayers[0]
+        const bounds = map.getBounds()
+
+        getFeatureInfo({
+          ne: bounds._northEast,
+          sw: bounds._southWest,
+          x: e.containerPoint.x,
+          y: e.containerPoint.y,
+          width: map._size.x,
+          height: map._size.y,
+          layer: layer.options.layers
+        })
+          .then(d => {
+            let value
+
+            if (d && d.properties.GRAY_INDEX && d.properties.GRAY_INDEX > -999.00) {
+              value = d.properties.GRAY_INDEX.toFixed(2)
+            }
+
+            if (value) {
+              L.popup()
+                .setLatLng(e.latlng)
+                .setContent(value)
+                .openOn(map)
+            }
+          })
+      }
+    })
 
     if (currentLayers.length) {
       currentLayers.forEach(layer => map.removeLayer(layer))
