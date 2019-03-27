273e8beef61009df602729524343a72009abe7ee
Sjoerd Beentjes <mail@sjoerdbeentjes.nl>
2019-03-27 13:29:24 +0100

    refactor(build-breach-notifications): replace loadBreachLayer url with generic url

--- src/lib/load-breach.js
+++ src/lib/load-breach.js
@@ -2,8 +2,7 @@ import { BREACH_PRIMARY, BREACH_REGIONAL } from '@/lib/liwo-identifiers'
 import mapConfig from '../map.config'
 
 const BREACHES_BASE_URL = mapConfig.services.WEBSERVICE_URL
-const BREACHES_API_URL_PRIMARY = `${BREACHES_BASE_URL}/Tools/FloodImage.asmx/GetScenariosPerBreach`
-const BREACHES_API_URL_REGIONAL = `${BREACHES_BASE_URL}/Tools/FloodImage.asmx/GetScenariosPerBreachRegional`
+const BREACHES_API_URL = `${BREACHES_BASE_URL}/Tools/FloodImage.asmx/GetScenariosPerBreachGeneric`
 const COMBINE = 'combine'
 const COMBINED = 'combined'
 
@@ -24,7 +23,7 @@ export default function (breachId, layerType, viewerType) {
   }
 
   if (layerType === BREACH_REGIONAL) {
-    return loadRegionalBreachLayer(breachId, `${BREACH_REGIONAL}.${breachId}`)
+    return loadBreachLayer(breachId, `${BREACH_REGIONAL}.${breachId}`)
   } else if (layerType === BREACH_PRIMARY) {
     return Promise.all(breachLayers.map(layerName => loadBreachLayer(breachId, layerName)))
       .then(layers => {
@@ -41,7 +40,7 @@ export default function (breachId, layerType, viewerType) {
 }
 
 function loadBreachLayer (breachid, layername) {
-  return fetch(BREACHES_API_URL_PRIMARY, {
+  return fetch(BREACHES_API_URL, {
     method: 'POST',
     mode: 'cors',
     credentials: 'omit',
@@ -55,20 +54,4 @@ function loadBreachLayer (breachid, layername) {
     .then(data => JSON.parse(data.d))
     .then(data => ({ ...data[0].layerset[0] }))
     .catch(() => undefined)
-}
-
-function loadRegionalBreachLayer (breachid, gid) {
-  return fetch(BREACHES_API_URL_REGIONAL, {
-    method: 'POST',
-    mode: 'cors',
-    headers,
-    body: JSON.stringify({
-      breachid,
-      gid
-    })
-  })
-    .then(res => res.json())
-    .then(data => JSON.parse(data.d))
-    .then(data => ({ ...data[0].layerset[0] }))
-    .catch()
 }
