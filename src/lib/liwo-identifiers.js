1c646c0eab6cfde3382afb82bc9221bd957860bc
Fedor Baart <fedor.baart@deltares.nl>
2019-05-15 17:31:34 +0200

    keep identifiers together

--- src/lib/liwo-identifiers.js
+++ src/lib/liwo-identifiers.js
@@ -4,6 +4,9 @@ export const BREACH_REGIONAL = 'geo_doorbraaklocaties_regionaal'
 export const BREACH_OUTSIDE_DIKE = 'geo_scenariolocaties_buitendijks'
 export const BREACH_FLOODING = 'geo_scenariolocaties_wateroverlast'
 export const BREACH_SELECTED = 'selected_marker'
+export const BREACHES_PRIMARY_LAYER_ID = 'geo_doorbraaklocaties_primair'
+export const BREACHES_REGIONAL_LAYER_ID = 'geo_doorbraaklocaties_regionaal'
+
 export const BREACHES_IDS = [
   BREACH_PRIMARY,
   BREACH_REGIONAL,
