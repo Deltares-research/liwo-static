14ced987b2a302cc0529445675a69d5b93282eb0
Fedor Baart <fedor.baart@deltares.nl>
2019-05-28 16:37:32 +0200

    compute breaches

--- src/lib/load-breach.js
+++ src/lib/load-breach.js
@@ -1,5 +1,7 @@
 import _ from 'lodash'
 
+import store from '@/store'
+
 import { BREACH_PRIMARY, BREACH_REGIONAL, getLayerType } from '@/lib/liwo-identifiers'
 import mapConfig from '../map.config'
 
@@ -8,8 +10,9 @@ const BREACHES_API_URL = `${BREACHES_BASE_URL}/Tools/FloodImage.asmx/GetScenario
 
 const headers = { 'Accept': 'application/json', 'Content-Type': 'application/json' }
 
-// Load breach data from the geoserver
-export default async function (feature) {
+export async function loadBreach (feature) {
+  // Load breach data from the geoserver
+
   // the breach id is hidden here
   let breachId = feature.properties.id
 
@@ -37,7 +40,7 @@ export default async function (feature) {
   // the layers are a bit out of order, so restructure them
   // TODO: consider making this async, otherwise we lock the browser
   let bands = await Promise.all(promises)
-  // remove undefined bands
+  // remove undefined/null bands
   bands = _.filter(bands)
 
   // merge layers of all unorganized sets
@@ -53,7 +56,54 @@ export default async function (feature) {
   return layerSet
 }
 
-function loadBreachLayer (breachid, layername) {
+export async function computeBreaches (features) {
+  // Load combined breaches map, computed by the backend
+  // The computation is done in Google Earth Engine / HydroEngine
+  // the breach id is hidden here
+  let breachIds = _.map(features, 'properties.id')
+
+  let breachLayersEn = {
+    'waterdepth': 'waterdiepte',
+    'velocity': 'stroomsnelheid',
+    'riserate': 'stijgsnelheid',
+    'damage': 'schade',
+    'fatalities': 'slachtoffers'
+  }
+
+  // load  all the variants
+  let promises = _.keys(breachLayersEn).map(
+    bandName => loadBreachesLayer(breachIds, bandName)
+  )
+  // the layers are a bit out of order, so restructure them
+  // TODO: consider making this async, otherwise we lock the browser
+  let bands = await Promise.all(promises)
+
+  // remove undefined/null bands
+  bands = _.filter(bands)
+
+  // convert bands to layerlike objects
+  let layers = _.map(bands, (band) => {
+    let title = _.get(breachLayersEn, band.band)
+    band.title = title
+    band.metadata = {}
+    let layer = {
+      id: band.mapid,
+      variants: [band],
+      title: title
+    }
+    return layer
+  })
+  let layerSet = {
+    id: breachIds.join(','),
+    features,
+    name: 'Combined scenario',
+    title: 'Combined scenario',
+    layers
+  }
+  return layerSet
+}
+
+function loadBreachLayer (breachId, layerName) {
   // Load the dataset  for a breach
   return fetch(BREACHES_API_URL, {
     method: 'POST',
@@ -61,13 +111,35 @@ function loadBreachLayer (breachid, layername) {
     credentials: 'omit',
     headers,
     body: JSON.stringify({
-      breachid,
-      layername
+      breachid: breachId,
+      layername: layerName
     })
   })
     .then(res => res.json())
   // get rid of some ASP  fluff
     .then(data => JSON.parse(data.d))
     .then(data => ({ ...data[0].layerset[0] }))
-    .catch(() => undefined)
+    .catch(() => null)
+}
+
+export default function loadBreachesLayer (breachIds, band) {
+  // TODO: choose appropriate reducer for the band
+  // The band here relates to  quantitites
+  let reducer = 'max'
+  const requestOptions = {
+    method: 'POST',
+    mode: 'cors',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ liwo_ids: breachIds, band, reducer })
+  }
+
+  return fetch('https://hydro-engine.appspot.com/get_liwo_scenarios', requestOptions)
+    .then(text => text.json())
+    .then(response => ({ ...response, type: 'tile' }))
+    .catch((resp) => {
+      let notification = `failed to load ${band} for breachIDs ${breachIds}`
+      // notifiy of failure
+      store.commit('addNotificationById', {id: breachIds.join(','), notification})
+      return null
+    })
 }
