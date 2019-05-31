b3d37e2bfbcf653a7752eba6bfa30a57750239b8
Fedor Baart <fedor.baart@deltares.nl>
2019-05-31 14:39:52 +0200

    add test for LayerControlList

---
+++ tests/unit/LayerControlList.spec.js
@@ -0,0 +1,27 @@
+import Vue from 'vue'
+import LayerControlList from '@/components/LayerControlList'
+import chai from 'chai'
+import chaiDom from 'chai-dom'
+
+chai.use(chaiDom)
+
+const expect = chai.expect
+
+describe('the LayerControlList', () => {
+  it('should have the layer-control-list class', () => {
+    const Constructor = Vue.extend(LayerControlList)
+    let propsData = {}
+    const vm = new Constructor({ propsData: propsData }).$mount()
+    expect(vm.$el).to.have.class('layer-control-list')
+  })
+  it('should have load layers', () => {
+    const Constructor = Vue.extend(LayerControlList)
+    let layers = [
+      {id: 3, properties: {}, variants: []},
+      {id: 5, properties: {}, variants: []}
+    ]
+    let propsData = { layers }
+    const vm = new Constructor({ propsData: propsData }).$mount()
+    expect(vm.$el).to.have.class('layer-control-list')
+  })
+})
