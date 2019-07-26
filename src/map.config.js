a1de589b1f3873ae3a333b9eac885fbc93fad98d
Fedor Baart <fedor.baart@deltares.nl>
2019-07-26 16:36:37 +0200

    load  services from webconfig.json

--- src/map.config.js
+++ src/map.config.js
@@ -2,56 +2,6 @@
 
 import { EPSG_28992, EPSG_3857 } from './lib/leaflet-utils/projections'
 
-// These variable names are global and should be defined by loading webconfig.js
-const globals = [
-  'WEBSERVICE_URL',
-  'STATIC_GEOSERVER_URL',
-  'DYNAMIC_GEOSERVER_URL',
-  'ACHTERGRONDKAART',
-  'LUCHTFOTOKAART',
-  'PDOKLUCHTFOTO',
-  'PDOKBRTACHTERGROND',
-  'MANUAL_URL',
-  'STATIC_GEOSERVER_URL',
-  'PRINT_GEOSERVER_URL',
-  'HYDRO_ENGINE_URL'
-]
-
-// Assertions
-globals.forEach(
-  function (v) {
-    console.assert(
-      // variable should be set by webconfig.js
-      window.hasOwnProperty(v),
-      // error message if assert fails
-      'Make sure variable',
-      v,
-      'is defined in the webconfig.js and that this file is properly loaded'
-    )
-  }
-)
-
-const services = {
-  // url for webservice calls [ending with a forward slash]
-  // We use the || shortcut here because we don't expect any falsy values
-  // TODO: consider removing this as a configuration all together
-  // url for static geoserver [ending with a forward slash]
-  WEBSERVICE_URL: window.WEBSERVICE_URL,
-  STATIC_GEOSERVER_URL: window.STATIC_GEOSERVER_URL,
-  DYNAMIC_GEOSERVER_URL: window.DYNAMIC_GEOSERVER_URL,
-  ACHTERGRONDKAART: window.ACHTERGRONDKAART,
-  LUCHTFOTOKAART: window.LUCHTFOTOKAART,
-  PDOKLUCHTFOTO: window.PDOKLUCHTFOTO,
-  PDOKBRTACHTERGROND: window.PDOKBRTACHTERGROND,
-  MANUAL_URL: window.MANUAL_URL,
-  // TODO: consistent names
-  LEGEND_URL: window.STATIC_GEOSERVER_URL,
-  // TODO: consistent names
-  PRINT_GEO_SERVER: window.PRINT_GEOSERVER_URL,
-  // add default, so it keeps working with old config files
-  HYDRO_ENGINE: window.HYDRO_ENGINE_URL || 'https://hydro-engine.appspot.com'
-}
-
 // This is the attribution (maps and datasets often require attribution of the source)
 const attribution = '&copy <a href="http://www.pdok.nl">PDOK</a>'
 
@@ -113,12 +63,26 @@ const tileLayers = [
   }
 ]
 
+var services = null
+
+async function getServices () {
+  // return the content of webconfig.json
+  // return if we already fetched it (memoize0
+  if (services) {
+    return services
+  }
+  let resp = await fetch('config/webconfig.json')
+  let result = await resp.json()
+  services = result
+  return result
+}
+
 export default {
   attribution,
   center,
   maxZoom,
   minZoom,
-  services,
+  getServices,
   tileLayers,
   zoom
 }
