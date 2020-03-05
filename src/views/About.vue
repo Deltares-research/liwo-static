ed46d22838d7d9c2496e140095012c6878aa0aef
Fedor Baart <fedor.baart@deltares.nl>
2020-03-05 13:12:33 +0100

    update version

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,44 +1,57 @@
 <template>
-  <div class="about site-main">
-    <div>
-      <section class="section">
-        <h2 data-class="section-head">Tips voor sitebezoek</h2>
-        <p>Voor gedetailleerde informatie over het gebruik van deze website verwijzen we u naar
-            de gebruikershandleiding. De handleiding vindt u op
-            de <a href="https://www.helpdeskwater.nl/onderwerpen/applicaties-modellen/applicaties-per/watermanagement/watermanagement/liwo/"
-            target="_blank" rel="nofollow noopener">LIWO pagina van de helpdesk water</a>.
-        </p>
-        <h2 data-class="section-head">Auteursrecht</h2>
-        <p>
-          Het auteursrecht op de LIWO-website is uitdrukkelijk
-          voorbehouden en berust bij het Watermanagementcentrum
-          Nederland (WMCN). Onder voorwaarde dat de bron (LIWO,
-          Watermanagementcentrum Nederland) wordt vermeld, mogen
-          onderdelen van de inhoud van de website worden
-          overgenomen.
-        </p>
-        <h2 data-class="section-head">Privacy</h2>
-        <p>
-          Uw persoonsgegevens worden conform de Wet Bescherming
-          Persoonsgegevens
-          behandeld.
-        </p>
-        <h2 data-class="section-head">Vernieuwing LIWO</h2>
-        <p>
-          Er wordt veel zorg besteed aan het zo compleet, correct, actueel en toegankelijk mogelijk aanbieden van de informatie in LIWO. Wij streven ernaar LIWO zo volledig en gebruikersvriendelijk mogelijk te houden. LIWO wordt steeds verder ontwikkeld en periodiek volgt er een nieuwe release.
-        </p>
-        <div class="panel soft" title="De broncode van deze software staat op github.com/deltares/liwo-static">versie: {{ version }}</div>
-      </section>
-    </div>
+<div class="about site-main">
+  <div>
+    <section class="section">
+      <h2 data-class="section-head">Tips voor sitebezoek</h2>
+      <p>Voor gedetailleerde informatie over het gebruik van deze website verwijzen we u naar
+        de gebruikershandleiding. De handleiding vindt u op
+        de <a href="https://www.helpdeskwater.nl/onderwerpen/applicaties-modellen/applicaties-per/watermanagement/watermanagement/liwo/"
+              target="_blank" rel="nofollow noopener">LIWO pagina van de helpdesk water</a>.
+      </p>
+      <h2 data-class="section-head">Auteursrecht</h2>
+      <p>
+        Het auteursrecht op de LIWO-website is uitdrukkelijk
+        voorbehouden en berust bij het Watermanagementcentrum
+        Nederland (WMCN). Onder voorwaarde dat de bron (LIWO,
+        Watermanagementcentrum Nederland) wordt vermeld, mogen
+        onderdelen van de inhoud van de website worden
+        overgenomen.
+      </p>
+      <h2 data-class="section-head">Privacy</h2>
+      <p>
+        Uw persoonsgegevens worden conform de Wet Bescherming
+        Persoonsgegevens
+        behandeld.
+      </p>
+      <h2 data-class="section-head">Vernieuwing LIWO</h2>
+      <p>
+        Er wordt veel zorg besteed aan het zo compleet, correct, actueel en toegankelijk mogelijk aanbieden van de informatie in LIWO. Wij streven ernaar LIWO zo volledig en gebruikersvriendelijk mogelijk te houden. LIWO wordt steeds verder ontwikkeld en periodiek volgt er een nieuwe release.
+      </p>
+      <div class="panel soft" title="applicatie (data + code)">
+        <p>applicatie versie: {{ version }}</p>
+        <p>user interface versie: {{ uiVersion }}</p>
+        <p>dataset versie: {{ datasetVersion }}</p>
+        <p>De broncode van deze software staat op <a href="https://github.com/deltares/liwo-static">github/deltares/liwo-static</a></p>
+      </div>
+
+    </section>
   </div>
+</div>
 </template>
 
 <script>
+
+console.log('env', process.env)
+
 export default {
   name: 'About',
   data () {
     return {
-      version: process.env.VUE_APP_VERSION
+      uiVersion: process.env.VUE_APP_VERSION,
+      // combined  version
+      version: process.env.VUE_APP_LIWO_VERSION,
+      datasetVersion: process.env.VUE_APP_DATASET_VERSION
+
     }
   }
 }
