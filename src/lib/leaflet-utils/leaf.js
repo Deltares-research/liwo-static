e0c5fb9d9c070d2b744423377d8f35e70127c0b7
Fedor Baart <fedor.baart@deltares.nl>
2020-05-05 12:08:52 +0200

    add dependency on liwo.browser.print

--- src/lib/leaflet-utils/leaf.js
+++ src/lib/leaflet-utils/leaf.js
@@ -4,6 +4,8 @@ import 'leaflet/dist/leaflet.css'
 import 'leaflet-control-geocoder'
 import 'leaflet-control-geocoder/dist/Control.Geocoder.css'
 
+import 'leaflet.browser.print/dist/leaflet.browser.print'
+
 import 'leaflet.markercluster'
 import 'proj4leaflet'
 
