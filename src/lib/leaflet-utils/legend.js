2edc117696c364875439278f06007fff376a8ad6
Fedor Baart <fedor.baart@deltares.nl>
2020-05-06 11:24:11 +0200

    typo

--- src/lib/leaflet-utils/legend.js
+++ src/lib/leaflet-utils/legend.js
@@ -3,7 +3,7 @@ import L from '@/lib/leaflet-utils/leaf'
 const LegendControl = L.Control.extend({
   onAdd: function (map) {
     var div = L.DomUtil.create('div', 'info legend')
-    // make a copy  of hte legend and add it to the print map
+    // make a copy of the legend and add it to the print map
     div.appendChild(this.options.el.cloneNode(true))
     return div
   }
