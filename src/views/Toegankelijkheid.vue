9cc8f96dc66377feb37c81f289a7488bbb3804a6
Peter Goes <petergoes@gmail.com>
2020-12-15 09:43:53 +0100

    Add links in text of Toegankelijkheid view

--- src/views/Toegankelijkheid.vue
+++ src/views/Toegankelijkheid.vue
@@ -3,16 +3,16 @@
     <div>
       <section class="section">
         <h2 data-class="section-head">Toegankelijkheid</h2>
-        <p>Rijkswaterstaat wil dat iedereen alle informatie op deze website goed kan gebruiken. Daarom besteden we veel aandacht aan gebruiksvriendelijkheid, toegankelijkheid en vindbaarheid. In het Tijdelijk besluit digitale toegankelijkheid overheid is bepaald dat overheidssites moeten voldoen aan toegankelijkheidseisen zoals vastgelegd in EN 301 549/WCAG 2.1 en daarover verantwoordelijkheid moeten afleggen in een gepubliceerde toegankelijkheidsverklaring.</p>
+        <p>Rijkswaterstaat wil dat iedereen alle informatie op deze website goed kan gebruiken. Daarom besteden we veel aandacht aan gebruiksvriendelijkheid, toegankelijkheid en vindbaarheid. <a href="https://wetten.overheid.nl/BWBR0040936/2018-07-01" rel="noopener noreferrer" target="_blank">In het Tijdelijk besluit digitale toegankelijkheid overheid</a> is bepaald dat overheidssites moeten voldoen aan toegankelijkheidseisen zoals vastgelegd in EN 301 549/WCAG 2.1 en daarover verantwoordelijkheid moeten afleggen in een gepubliceerde toegankelijkheidsverklaring.</p>
 
         <h2 data-class="section-head">Toegankelijkheidsverklaring</h2>
         <p>
-          Basisinformatie-overstromingen.rws.nl (LIWO) voldoet gedeeltelijk aan de eisen op het gebied van toegankelijkheid. In onze Toegankelijkheidsverklaring lichten wij toe op welke punten wij nog niet volledig voldoen en op welke termijn wij verwachten de knelpunten op te lossen.
+          Basisinformatie-overstromingen.nl (LIWO) voldoet gedeeltelijk aan de eisen op het gebied van toegankelijkheid. In onze <a href="https://www.toegankelijkheidsverklaring.nl/register/2902" rel="noopener noreferrer" target="_blank">Toegankelijkheidsverklaring</a> lichten wij toe op welke punten wij nog niet volledig voldoen en op welke termijn wij verwachten de knelpunten op te lossen.
           <a href="https://www.toegankelijkheidsverklaring.nl/register/2902"><img src="https://www.toegankelijkheidsverklaring.nl/files/verklaring/label/24d6cf59411ecf76f07b7125e1e86faa.2902.svg" alt="Het toegankelijkheidslabel van LIWO. Volg de link voor de toegankelijke versie van dit label." width="692" height="251" /></a>
         </p>
 
         <h2 data-class="section-head">Onderbouwing bij de toegankelijkheidsverklaring</h2>
-        <p>De toegankelijkheidsverklaring voor Basisinformatie-overstromingen.rws.nl (LIWO) is opgesteld op basis van onderzoek door een onafhankelijk toetsingsbureau. De bevindingen hiervan zijn opgenomen in het meest recente onderzoeksrapport.</p>
+        <p>De toegankelijkheidsverklaring voor Basisinformatie-overstromingen.nl (LIWO) is opgesteld op basis van onderzoek door een onafhankelijk toetsingsbureau. De bevindingen hiervan zijn opgenomen in het meest recente <a href="https://www.helpdeskwater.nl/publish/pages/184976/check_8472_auto_rapport.pdf" rel="noopener noreferrer" target="_blank">onderzoeksrapport</a>.</p>
 
         <h2 data-class="section-head">Hoe werken wij aan toegankelijkheid?</h2>
         <p>Rijkswaterstaat zorgt voor een continue verbetering in toegankelijkheid door diverse maatregelen binnen onze (dagelijkse) processen:
@@ -24,10 +24,10 @@
         </p>
 
         <h2 data-class="section-head">Overzicht toegankelijkheidsverklaringen Rijkswaterstaat</h2>
-        <p>Toegankelijkheidsverklaringen van andere websites dan basisinformatie-overstromingen.rws.nl die eveneens onder verantwoordelijkheid vallen van Rijkswaterstaat, kunt u bekijken in het register van toegankelijkheidsverklaringen.</p>
+        <p>Toegankelijkheidsverklaringen van andere websites dan Basisinformatie-overstromingen.nl die eveneens onder verantwoordelijkheid vallen van Rijkswaterstaat, kunt u bekijken in het <a href="https://www.toegankelijkheidsverklaring.nl/register" rel="noopener noreferrer" target="_blank">register van toegankelijkheidsverklaringen</a>.</p>
 
         <h2 data-class="section-head">Problemen met toegankelijkheid melden</h2>
-        <p>Heeft u vragen of opmerkingen? Of wilt u een pagina gebruiken op deze website die niet toegankelijk blijkt? Neem dan contact met ons op</p>
+        <p>Heeft u vragen of opmerkingen? Of wilt u een pagina gebruiken op deze website die niet toegankelijk blijkt? Neem dan <a href="https://www.helpdeskwater.nl/stel-vraag/" rel="noopener noreferrer" target="_blank">contact met ons</a> op</p>
       </section>
     </div>
   </div>
