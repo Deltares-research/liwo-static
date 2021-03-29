bbc814e2fd7311ae920c8e828e23c921a4d8a753
Fedor Baart <fedor.baart@deltares.nl>
2021-03-29 18:44:55 +0200

    fix(test:e2e): fix e2e mocha issue (dowgrade mocha)

--- tests/e2e/plugins/index.js
+++ tests/e2e/plugins/index.js
@@ -16,6 +16,7 @@ module.exports = (on, config) => {
   // }))
 
   return Object.assign({}, config, {
+    defaultCommandTimeout: 8000,
     fixturesFolder: 'tests/e2e/fixtures',
     integrationFolder: 'tests/e2e/specs',
     screenshotsFolder: 'tests/e2e/screenshots',
