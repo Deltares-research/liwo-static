a0dc1019db378fe532155c0317116a06ae05c34e
Sjoerd Beentjes <mail@sjoerdbeentjes.nl>
2021-09-08 14:11:59 +0200

    feat: add test for images

---
+++ tests/e2e/specs/images.js
@@ -0,0 +1,28 @@
+const pages = ['/', '#/contact', '#/accessibility', '#/about']
+
+describe('Images', () => {
+  it('Has images with alt attributes', () => {
+    pages.forEach(page => {
+      cy.visit(page)
+
+      cy.get('img').each($a => {
+        expect($a).to.have.attr('alt')
+      })
+    })
+  })
+
+  it("Contains images with valid url's", () => {
+    pages.forEach(page => {
+      cy.visit(page)
+
+      cy.get('img').each($a => {
+        const src = $a.prop('src')
+
+        cy.request(src)
+          .should((response) => {
+            expect(response.status).to.eq(200)
+          })
+      })
+    })
+  })
+})
