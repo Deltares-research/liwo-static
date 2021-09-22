d9757216b0d7f7c52f13fefcbdd8bb02a85b95c3
Sjoerd Beentjes <mail@sjoerdbeentjes.nl>
2021-09-22 16:59:30 +0200

    feat: add tests for layer-controls

---
+++ tests/e2e/specs/layer-controls.js
@@ -0,0 +1,23 @@
+import { generateSelector as selector } from '../lib/generate-selector'
+
+describe('Layers', () => {
+  it('Changes opacity of layer', () => {
+    const url = '#/viewer/1?center=52.15382,4.88242&zoom=2'
+    cy.visit(url)
+
+    cy.get(`${selector('transparancy-input')} input`)
+      .invoke('val', 0.5)
+      .trigger('change')
+
+    const opacityValues = []
+
+    cy.get('.leaflet-layer').each(layer => {
+      cy.get(layer).invoke('css', 'opacity').then(value => opacityValues.push(value))
+    })
+
+    cy.wait(10).then(() => {
+      // eslint-disable-next-line no-unused-expressions
+      expect(opacityValues.some(value => value === '0.5')).to.be.true
+    })
+  })
+})
