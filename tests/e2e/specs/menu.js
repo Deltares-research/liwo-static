a338792d4db2888bc4ecca0f14404c9b509caf1a
Sjoerd Beentjes <sjoerd@voorhoede.nl>
2021-10-06 11:47:48 +0200

    test: menu (#197)
    * feat: add v-test directive
    
    * feat: add test for menu
    
    * feat: add test for page titles
    
    * remove dummy test

---
+++ tests/e2e/specs/menu.js
@@ -0,0 +1,35 @@
+const pages = [
+  ['Home', '#/'],
+  ['Kaarten', '#/maps'],
+  ['Over LIWO', '#/about']
+]
+
+describe('Menu', () => {
+  it('Contains correct pages', () => {
+    cy.visit('/')
+
+    cy.get('nav').within(() => {
+      pages.forEach(([title]) => {
+        cy.get('a').contains(title).should('be.visible')
+      })
+    })
+  })
+  it('Navigates to the correct pages', () => {
+    cy.visit('/')
+
+    cy.get('nav').within(() => {
+      pages.forEach(([title, url]) => {
+        cy.get('a').contains(title).click()
+        cy.location().should((loc) => {
+          expect(loc.hash).to.eq(url)
+        })
+      })
+    })
+  })
+  it('Has correct page titles', () => {
+    pages.forEach(([title, url]) => {
+      cy.visit(url)
+      cy.get('header h1').contains(title)
+    })
+  })
+})
