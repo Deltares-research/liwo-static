8508f3b7f85217b52e64d257515bbb7431911420
Sjoerd Beentjes <mail@sjoerdbeentjes.nl>
2022-01-05 13:58:43 +0100

    feat(e2e): base for layer-specific tests

---
+++ tests/e2e/lib/get-layers.js
@@ -0,0 +1,18 @@
+const layers = [
+  {
+    url: '/#/viewer/18',
+    id: 'plaatsgebonden_kans_totaal_huidig_0'
+  },
+  {
+    url: '/#/viewer/18',
+    id: 'overstromingskansen_actueel_2019_faalkans_primair'
+  },
+  {
+    url: '/#/viewer/18',
+    id: 'overstromingskansen_actueel_faalkans_regionaal'
+  }
+]
+
+export function getLayers () {
+  return layers
+}
---
+++ tests/e2e/specs/layer/map-layers.js
@@ -0,0 +1,68 @@
+import { getLayers } from '../../lib/get-layers'
+import { generateSelector as selector } from '../../lib/generate-selector'
+import { skipOn } from '@cypress/skip-test'
+
+const layers = getLayers()
+
+// selects specified layer in layer list
+// should at least be called on first test for layer
+function selectLayer(cy, layer) {
+  cy.url().then((currentUrl) => {
+    if (!currentUrl.includes(`${layer.url}?`)) {
+      cy.intercept(new RegExp(/GetLayerSet/)).as('layerset')
+
+      cy.visit(layer.url)
+
+      cy.wait('@layerset')
+    }
+
+    // disable all layers
+    cy.get('.layer-control__vis-checkbox').each(el => {
+      if (el[0].checked) {
+        el[0].click({ force: true })
+      }
+    })
+
+    // enable one specific layer that we want to test
+    cy.get(`[data-id="${layer.id}"] input[type="checkbox"]`).click({ force: true })
+  })
+}
+
+describe('Layer functionalities', () => {
+  layers.forEach(layer => {
+    it('exports zip file', () => {
+      selectLayer(cy, layer)
+
+      const fileName = 'test-filename'
+
+      cy.intercept(new RegExp(/DownloadZipFileDataLayers/))
+        .as('apiCheck')
+
+      cy.get(selector('init-export-button'))
+        .click()
+
+      cy.get(selector('name-input'))
+        .type(fileName)
+
+      cy.get(selector('export-file-button'))
+        .click()
+        .wait('@apiCheck')
+        .should((xhr) => {
+          const body = xhr.request.body
+          expect(body.name).to.equal(fileName)
+        })
+        .then(({ response }) => {
+          // size in bytes
+          const size = Number(response.headers['content-length'])
+
+          expect(size).to.be.greaterThan(5000)
+        })
+
+      cy.get(selector('close-button')).click()
+    })
+
+    it('Renders legend', () => {
+      cy.get(selector('legend')).should('exist')
+    })
+  })
+})
