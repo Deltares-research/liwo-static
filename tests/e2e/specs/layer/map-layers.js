9d9bae5a34135f0d1bc4269014d33b478f35f693
Sjoerd Beentjes <sjoerd@voorhoede.nl>
2022-01-07 13:26:30 +0100

    Use CSV as source for layer tests (#271)
    * feat(e2e): use CSV as source for layer tests
    
    * fix(e2e): adjust metadata test selector
    
    * style(e2e): fix double quotes

--- tests/e2e/specs/layer/map-layers.js
+++ tests/e2e/specs/layer/map-layers.js
@@ -1,36 +1,38 @@
-import { getLayers } from '../../lib/get-layers'
 import { generateSelector as selector } from '../../lib/generate-selector'
 import { getLayerInfoValue } from '../../../../src/lib/leaflet-utils/get-layer-info-value'
-
-const layers = getLayers()
 
 // selects specified layer in layer list
 // should at least be called on first test for layer
 function selectLayer (cy, layer) {
-  cy.url().then((currentUrl) => {
-    if (!currentUrl.includes(`${layer.url}?`)) {
-      cy.intercept(new RegExp(/GetLayerSet/)).as('layerset')
+  cy.intercept(new RegExp(/GetLayerSet/)).as('layerset')
 
-      cy.visit(layer.url)
+  cy.visit('#/maps')
 
-      cy.wait('@layerset')
+  cy.contains(layer['Kaartenset']).click()
+
+  cy.wait('@layerset')
+
+  // disable all layers
+  cy.get('.layer-control__vis-checkbox').each(el => {
+    if (el[0].checked) {
+      el[0].click({ force: true })
     }
-
-    // disable all layers
-    cy.get('.layer-control__vis-checkbox').each(el => {
-      if (el[0].checked) {
-        el[0].click({ force: true })
-      }
-    })
-
-    // enable one specific layer that we want to test
-    cy.get(`[data-id="${layer.id}"] input[type="checkbox"]`).click({ force: true })
   })
+
+  // enable one specific layer that we want to test
+  cy.get(`[data-name="${layer['Kaartlaag']}"] input[type="checkbox"]`).click({ force: true })
+
+  if (layer['Variant']) {
+    cy.get(`[data-name="${layer['Kaartlaag']}"]`)
+      .within(() => {
+        cy.get(selector('variant-select')).select(layer['Variant'].trim())
+      })
+  }
 }
 
 describe('Layer functionalities', () => {
-  layers.forEach(layer => {
-    it('Exports zip file', () => {
+  Cypress.env('MAP_LAYERS').forEach(layer => {
+    it('exports zip file', () => {
       selectLayer(cy, layer)
 
       const fileName = 'test-filename'
@@ -38,27 +40,32 @@ describe('Layer functionalities', () => {
       cy.intercept(new RegExp(/DownloadZipFileDataLayers/))
         .as('apiCheck')
 
-      cy.get(selector('init-export-button'))
-        .click()
-
-      cy.get(selector('name-input'))
-        .type(fileName)
-
-      cy.get(selector('export-file-button'))
-        .click()
-        .wait('@apiCheck')
-        .should((xhr) => {
-          const body = xhr.request.body
-          expect(body.name).to.equal(fileName)
-        })
-        .then(({ response }) => {
-          // size in bytes
-          const size = Number(response.headers['content-length'])
-
-          expect(size).to.be.greaterThan(5000)
-        })
-
-      cy.get(selector('close-button')).click()
+      cy.get('body').then($body => {
+        // only run if export button exists
+        if ($body.find(selector('init-export-button')).length) {   
+          cy.get(selector('init-export-button'))
+            .click()
+    
+          cy.get(selector('name-input'))
+            .type(fileName)
+    
+          cy.get(selector('export-file-button'))
+            .click()
+            .wait('@apiCheck')
+            .should((xhr) => {
+              const body = xhr.request.body
+              expect(body.name).to.equal(fileName)
+            })
+            .then(({ response }) => {
+              // size in bytes
+              const size = Number(response.headers['content-length'])
+    
+              expect(size).to.be.greaterThan(5000)
+            })
+    
+          cy.get(selector('close-button')).click()
+        }
+      });
     })
 
     it('renders legend', () => {
@@ -80,9 +87,7 @@ describe('Layer functionalities', () => {
     })
 
     it('shows metadata modal', () => {
-      selectLayer(cy, layer)
-
-      cy.get(`[data-id="${layer.id}"]`)
+      cy.get(`[data-name="${layer['Kaartlaag']}"]`)
         .within(() => {
           cy.get(selector('info-toggle')).first().click({ force: true })
 
@@ -95,6 +100,10 @@ describe('Layer functionalities', () => {
 
           cy.get(selector('close-button')).click()
         })
+    })
+    
+    it('renders layer correctly', () => {
+      cy.screenshot(`${layer['Kaartenset']}__${layer['Kaartlaag']}__${layer['Variant']}`)
     })
   })
 })
