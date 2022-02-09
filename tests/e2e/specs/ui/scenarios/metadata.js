ee507a29897a5a42a9d88c0034afa0d4591fe4f8
Fedor Baart <fedor.baart@deltares.nl>
2022-02-09 15:46:41 +0100

    linting

--- tests/e2e/specs/ui/scenarios/metadata.js
+++ tests/e2e/specs/ui/scenarios/metadata.js
@@ -8,15 +8,15 @@ describe('Scenarios: metadata is available', () => {
       cy.viewport(1500, 1000)
       cy.visit(url)
       cy.intercept(new RegExp(/getFeature/)).as('features')
-  
+
       cy.wait('@features', { timeout: 20000 })
-  
+
       cy.wait(8000)
-  
+
       cy.get(selector('layer-panel')).eq(1)
         .within(() => {
           cy.get(selector('info-toggle')).first().click()
-  
+
           cy.get(selector('meta-table'))
             .contains('Scenarionaam')
             .contains('ScenarioID')
