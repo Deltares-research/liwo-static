64693a9a282b0ce67cc65c42281ddca3f3c8ca12
Vincent van Dijck <vincent@voorhoede.nl>
2022-10-25 14:26:50 +0200

    fix(e2e): replace combined scenario

--- tests/e2e/specs/ui/combine/replace-scenarios.js
+++ tests/e2e/specs/ui/combine/replace-scenarios.js
@@ -11,16 +11,19 @@ const location2 = mockDoubleFeaturesData.features[1].properties.name
 
 describe('Combine: Export and import combined scenarios', () => {
   beforeEach(() => {
-    cy.intercept(new RegExp(/GetLayerSet/), mockLayerSetData)
+    cy.intercept(new RegExp(/GetLayerSet/), mockLayerSetData).as('layerset')
     cy.intercept(new RegExp(/getFeature/), mockMultipleFeaturesData).as('features')
 
     cy.visit(url)
 
+    cy.wait('@layerset', { timeout: 20000 })
     cy.wait('@features', { timeout: 20000 })
 
     cy.get('.leaflet-marker-icon')
       .eq(0)
       .click({ force: true })
+
+    cy.wait(400)
 
     cy.get('.leaflet-marker-icon')
       .eq(4)
@@ -29,7 +32,7 @@ describe('Combine: Export and import combined scenarios', () => {
 
   it('Replaces selection with import', () => {
     cy.url()
-      .should('contain', '/combine/7/19428,19422', { timeout: 30000 })
+      .should('contain', '/combine/7/4922,12617', { timeout: 30000 })
       .then(() => {
         cy.contains(location1)
           .parentsUntil(selector('layer-panel'))
@@ -52,7 +55,7 @@ describe('Combine: Export and import combined scenarios', () => {
       })
 
     cy.url()
-      .should('contain', '/combine/7/19428,19422', { timeout: 30000 })
+      .should('contain', '/combine/7/4922,12617', { timeout: 30000 })
       .then(() => {
         cy.contains(location1)
           .parentsUntil(selector('layer-panel'))
@@ -64,7 +67,7 @@ describe('Combine: Export and import combined scenarios', () => {
 
   it('Combines import with selection', () => {
     cy.url()
-      .should('contain', '/combine/7/19428,19422', { timeout: 30000 })
+      .should('contain', '/combine/7/4922,12617', { timeout: 30000 })
       .then(() => {
         cy.contains(location1)
           .parentsUntil(selector('layer-panel'))
@@ -84,6 +87,6 @@ describe('Combine: Export and import combined scenarios', () => {
       })
 
     cy.url()
-      .should('contain', '/combine/7/19428,19422,19431,19435', { timeout: 30000 })
+      .should('contain', '/combine/7/4922,12617,19431,19435', { timeout: 30000 })
   })
 })
