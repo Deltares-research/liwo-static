04a0ce1eef9a577ff9fd6b0cd1d36fbeeca3bdef
sjoerdbeentjes <11621275+sjoerdbeentjes@users.noreply.github.com>
2022-11-01 16:24:57 +0100

    add e2e tests for variants

---
+++ tests/e2e/specs/ui/scenarios/variations.js
@@ -0,0 +1,48 @@
+const getUrl = (variantId) => `#/scenarios/6/${variantId}?center=51.84278,4.66138&zoom=13`
+
+const variants = [
+  {
+    id: '19784',
+    values: {
+      0: '0'
+    }
+  },
+  {
+    id: '19780',
+    values: {
+      0: '1'
+    }
+  },
+  {
+    id: '19783',
+    values: {
+      0: '2'
+    }
+  },
+  {
+    id: '19779',
+    values: {
+      0: '3'
+    }
+  }
+]
+
+describe('Scenarios: marker selection', () => {
+  variants.forEach((variant) => {
+    it(`Should show the correct input value for variant (${variant.id})`, () => {
+      cy.intercept(new RegExp(/GetLayerSet/)).as('layerset')
+      cy.intercept(new RegExp(/getFeature/)).as('features')
+
+      cy.reload()
+
+      cy.visit(getUrl(variant.id))
+
+      cy.wait('@layerset', { timeout: 20000 })
+      cy.wait('@features', { timeout: 20000 })
+
+      Object.entries(variant.values).forEach(([key, value]) => {
+        cy.get('.layer-control__options select').eq(key).should('have.value', value)
+      })
+    })
+  })
+})
