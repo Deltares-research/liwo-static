53469cf3e94397ac8bbc4ec6916c458c3578c9d2
Vincent van Dijck <vincent@voorhoede.nl>
2022-11-02 10:56:40 +0100

    feat(combine): get combined layer popup value

--- src/lib/leaflet-utils/popup.js
+++ src/lib/leaflet-utils/popup.js
@@ -2,6 +2,8 @@ import L from '@/lib/leaflet-utils/leaf'
 import getFeatureInfo from '@/lib/get-feature-info'
 import { getLayerInfoValue } from './get-layer-info-value'
 
+import mapConfig from '../../map.config.js'
+
 export function showLayerInfoPopup ({ map, layerId, unit, selectedLayer, position, latlng }) {
   const bounds = map.getBounds()
 
@@ -25,4 +27,35 @@ export function showLayerInfoPopup ({ map, layerId, unit, selectedLayer, positio
           .openOn(map)
       }
     })
+}
+
+export async function showCombinedLayerInfoPopup ({ coordinates, map }) {
+  const services = await mapConfig.getServices()
+  const HYDRO_ENGINE_URL = services.HYDRO_ENGINE_URL_V2
+  const { lat, lng } = coordinates
+  const url = `${HYDRO_ENGINE_URL}/get_feature_info`
+  const options = {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({
+      imageId: 'projects/dgds-gee/glossis/waterlevel/glossis_waterlevel_20200301000000',
+      bbox: {
+        type: 'Point',
+        coordinates: [lat, lng]
+      },
+      band: 'water_level_surge'
+    })
+  }
+
+  fetch(url, options)
+    .then(response => response.json())
+    .then(({ value }) => {
+      if (value !== null) {
+        L.popup()
+          .setLatLng(coordinates)
+          .setContent(`${value}`)
+          .openOn(map)
+      }
+    })
+    .catch(error => console.log('error', error))
 }
