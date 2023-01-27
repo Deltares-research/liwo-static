4d69f2d6bc7806d04dd69a22238a0b9ad02e1447
Vincent van Dijck <vincent@voorhoede.nl>
2023-01-27 16:18:26 +0100

    feat(root): add .eslintrc

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true
+  },
+  extends: [
+    // add more generic rulesets here, such as:
+    'eslint:recommended',
+    // 'plugin:vue/vue3-recommended',
+    'plugin:vue/recommended' // Use this if you are using Vue.js 2.x.
+  ],
+  rules: {
+    'vue/multi-word-component-names': 'off'
+  }
+}
