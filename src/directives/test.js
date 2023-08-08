18fe38847991bff8c995342faafbd78b85b36e11
Remco <info@remcokrams.nl>
2023-08-08 15:18:29 +0200

    fix: directive related stuff

--- src/directives/test.js
+++ src/directives/test.js
@@ -1,16 +1,18 @@
-import Vue from 'vue'
-
-Vue.directive('test', {
-  bind (el, binding) {
-    setAttribute(el, binding)
-  },
-  update (el, binding) {
-    setAttribute(el, binding)
-  }
-})
+const install = (app) => {
+  app.directive('test', {
+    created (el, binding) {
+      setAttribute(el, binding)
+    },
+    updated (el, binding) {
+      setAttribute(el, binding)
+    }
+  })
+}
 
 function setAttribute (el, binding) {
   if (binding.value !== false) {
     el.setAttribute('data-test', binding.value)
   }
 }
+
+export default install
--- src/lib/leaflet-directive.js
+++ src/lib/leaflet-directive.js
@@ -6,66 +6,73 @@ import mapFactory from './leaflet-utils/map-factory'
 
 let map
 let layerGroup
-// TODO: replace  this with vue2-leaflet
-// see vue docs
-// https://vuejs.org/v2/guide/components-edge-cases.html#Accessing-the-Parent-Component-Instance
-export default {
-  bind (el, { value }, vnode) {
-    const { config, callbacks } = value
-    map = mapFactory(el, vnode, config)
-    layerGroup = L.layerGroup().addTo(map)
-    callbacks.initMapObject(map)
-  },
-  update (_, { value, oldValue }, vnode) {
-    // check if of one of the layers the opacity changed
-    const changedOpacityLayers = value.layers.filter((layer) => {
-      // lookup the old layer in the old values
-      const oldLayer = oldValue.layers.find(l => l.layerObj.id === layer.layerObj.id)
 
-      if (oldLayer) {
-        const opacity = layer.layerObj.properties.opacity
-        const oldOpacity = oldLayer.layerObj.properties.opacity
+const install = (app) => {
+  app.directive('leaflet', {
 
-        return opacity !== oldOpacity
-      } else {
-        return false
-      }
-    })
+    mounted (el, { value }, vnode) {
+      const { config, callbacks } = value
+      map = mapFactory(el, vnode, config, callbacks.onPrint)
+      layerGroup = L.layerGroup().addTo(map)
+      callbacks.initMapObject(map)
+    },
 
-    // manually change opacity of the layers where opacity changed
-    // (this is very specific behaviour, but it improves the UX so much that it's worth it)
-    if (changedOpacityLayers.length) {
-      changedOpacityLayers.forEach(layer => {
-        const mapLayer = layerGroup.getLayers().find(l => l.options.layers === layer.layer)
-        mapLayer.setOpacity(layer.layerObj.properties.opacity)
-      })
+    updated (_, { value, oldValue }, vnode) {
+      // check if of one of the layers the opacity changed
+      const changedOpacityLayers = value.layers.filter((layer) => {
+        // lookup the old layer in the old values
+        const oldLayer = oldValue.layers.find(l => l.layerObj.id === layer.layerObj.id)
 
-      return
-    }
+        if (oldLayer) {
+          const opacity = layer.layerObj.properties.opacity
+          const oldOpacity = oldLayer.layerObj.properties.opacity
 
-    if (deepEqual(value, oldValue)) {
-      return
-    }
-    // TODO: use vue2-leaflet so we don't have to pass a magic  attribute
-
-    const { callbacks, cluster } = value
-    // TODO: why? remove falsy values?
-    const layers = value.layers.filter(value => value)
-
-    layerGroup.clearLayers()
-
-    const leafletLayers = layers
-      .filter(layer => !layer.hide)
-
-    leafletLayers
-      .map(layer => createLayer(layer, callbacks, cluster, vnode))
-      .filter(layer => layer)
-      .forEach(async layer => {
-        if (isPromise(layer)) {
-          layer = await layer
+          return opacity !== oldOpacity
+        } else {
+          return false
         }
-
-        layerGroup.addLayer(layer)
       })
-  }
+
+      // manually change opacity of the layers where opacity changed
+      // (this is very specific behaviour, but it improves the UX so much that it's worth it)
+      if (changedOpacityLayers.length) {
+        changedOpacityLayers.forEach(layer => {
+          const mapLayer = layerGroup.getLayers().find(l => l.options.layers === layer.layer)
+
+          // I added this check because sometimes mapLayer returned undefined
+          if(mapLayer) {
+            mapLayer.setOpacity(layer.layerObj.properties.opacity)
+          }
+        })
+
+        return
+      }
+
+      if (deepEqual(value, oldValue)) {
+        return
+      }
+      const { callbacks, cluster } = value
+
+      // TODO: why? remove falsy values?
+      const layers = value.layers.filter(value => value)
+
+      layerGroup.clearLayers()
+
+      const leafletLayers = layers
+        .filter(layer => !layer.hide)
+
+      leafletLayers
+        .map(layer => createLayer(layer, callbacks, cluster, vnode))
+        .filter(layer => layer)
+        .forEach(async layer => {
+          if (isPromise(layer)) {
+            layer = await layer
+          }
+
+          layerGroup.addLayer(layer)
+        })
+    }
+  })
 }
+
+export default install
