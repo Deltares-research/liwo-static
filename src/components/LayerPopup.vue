90dba345144731d44279fd50d8dd7c4491a7b863
Remco <info@remcokrams.nl>
2023-08-21 14:49:26 +0200

    fix: fix metadata popup

--- src/components/LayerPopup.vue
+++ src/components/LayerPopup.vue
@@ -1,22 +1,21 @@
 <template>
-  <pop-up class="layer-meta" title="Metadata" @close="$emit('close')">
+  <pop-up class="layer-meta" title="Metadata" @popup="$emit('close')">
     <template v-slot:icon>
       <svg class="icon" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 64 64">
         <path fill="none" d="M0 0h64v64H0z"/>
         <path d="M53.9 14.1c-.4-2-2-3.6-4-4-6-1-16-1-17.9-1-2 0-12 0-17.9 1-2 .4-3.6 2-4 4-1 6-1 16-1 17.9s0 12 1 17.9c.4 2 2 3.6 4 4 6 1 16 1 17.9 1 2 0 12 0 17.9-1 2-.4 3.6-2 4-4 1-6 1-16 1-17.9 0-6 0-12-1-17.9zM35 48h-6.6l.6-14v-8h6v22zm-3-26c-2.2 0-3.5-1.3-3.5-3.5 0-2 1.2-3.5 3.5-3.5 2.2 0 3.5 1.2 3.5 3.5 0 2-1.2 3.5-3.5 3.5z"/>
       </svg>
     </template>
-    <template>
-      <table class="layer-meta__table" v-test="'meta-table'">
-        <tr
-          v-for="(value, key) in noIdMetadata"
-          :key="key"
-        >
-          <th>{{titleCase(key)}}</th>
-          <td v-html="value"></td>
-        </tr>
-      </table>
-    </template>
+
+    <table class="layer-meta__table" v-test="'meta-table'">
+      <tr
+        v-for="(value, key) in noIdMetadata"
+        :key="key"
+      >
+        <th>{{titleCase(key)}}</th>
+        <td v-html="value"></td>
+      </tr>
+    </table>
   </pop-up>
 </template>
 
