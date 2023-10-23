a667ddc7945678a4915625e324604556f4a104d7
Remco <info@remcokrams.nl>
2023-10-23 14:48:33 +0200

    fix: lint issues

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,9 +3,20 @@ module.exports = {
   root: true,
   'extends': [
     'plugin:vue/vue3-essential',
-    'eslint:recommended'
+    'eslint:recommended',
+    'plugin:cypress/recommended'
   ],
+  env: {
+    "browser": true,
+    "node": true,
+  },
   parserOptions: {
     ecmaVersion: 'latest'
+  },
+  rules: {
+    "cypress/no-unnecessary-waiting": 0,
+    "cypress/unsafe-to-chain-command": 0,
+    "no-unused-vars": 1,
+    "vue/no-mutating-props": 0
   }
 }
