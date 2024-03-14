ac0dfdb58f6a545033eace0806648462666aa529
velomovies <22169955+velomovies@users.noreply.github.com>
2024-03-14 16:37:33 +0100

    fix(build-layerset-notifications): add ability to have notify from correct property

--- src/lib/build-layerset-notifications.js
+++ src/lib/build-layerset-notifications.js
@@ -2,31 +2,11 @@ import _ from 'lodash'
 
 function buildLayerSetFeatureNotifications (layers) {
   // create a list of notifications based on the layerset features, the feature that was  used to create the layerSet
-  let layerSetFeatureNotifications = layers
-    .filter(layer =>
-      layer.layerSet?.feature?.properties?.notify ||
-      layer.layerObj?.properties?.notify
-    )
+  const layerSetFeatureNotifications = layers
+    .filter(layer => layer.layerSet?.feature?.properties?.notify)
     .map((layer) => {
-      return {
-        id: layer.layerSet.id,
-        message:
-          layer.layerSet?.feature?.properties?.notify ||
-          layer.layerObj?.properties?.notify,
-        show: true,
-        type: 'info'
-      }
+      return buildNotification(layer.layerSet.id, layer.layerSet?.feature?.properties?.notify)
     })
-  layerSetFeatureNotifications = _.filter(layerSetFeatureNotifications)
-  // filter out messages that
-  // contain NULLs
-  layerSetFeatureNotifications = layerSetFeatureNotifications.filter(feature => {
-    if (feature.message === 'NULL') {
-      return false
-    } else {
-      return true
-    }
-  })
   return layerSetFeatureNotifications
 }
 
@@ -35,27 +15,42 @@ function buildVariantNotifications (layers) {
   const variantNotifications = layers
     .filter(layer => layer.variantNotification)
     .map(layer => {
-      return {
-        id: layer.layer,
-        message: layer.variantNotification,
-        show: true,
-        type: 'info'
-      }
+      return buildNotification(layer.layer, layer.variantNotification)
     })
   return variantNotifications
+}
+
+function buildLayerSetNotifications (layers) {
+  const layerSetNotifications = layers
+    .filter(layer => layer.layerSet?.notify)
+    .map(layer => {
+      return buildNotification(layer.layerSet.id, layer.layerSet?.notify)
+    })
+  return layerSetNotifications
 }
 
 export default function buildNotifications (layers) {
   const layerSetFeatureNotifications = buildLayerSetFeatureNotifications(layers)
   const variantNotifications = buildVariantNotifications(layers)
+  const layerSetNotifications = buildLayerSetNotifications(layers)
 
   // concatenate all features
   let result = [
     ...layerSetFeatureNotifications,
-    ...variantNotifications
+    ...variantNotifications,
+    ...layerSetNotifications
   ]
 
   // remove any doubles
   result = _.uniqWith(result, _.isEqual)
   return result
+}
+
+function buildNotification(id, message) {
+  return {
+    id,
+    message,
+    show: true,
+    type: 'info'
+  }
 }
