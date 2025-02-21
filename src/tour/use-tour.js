39cfaef83d7abf38d3524d0da7895197d9d809cb
velomovies <22169955+velomovies@users.noreply.github.com>
2025-02-21 12:00:13 +0100

    feat(use-tour): make sure you can't close the popups with click outside

--- src/tour/use-tour.js
+++ src/tour/use-tour.js
@@ -16,13 +16,22 @@ export function useTour(steps) {
     showProgress: true,
     progressText: "{{current}} van {{total}}",
     popoverOffset: 20,
-    showButtons: ["next", "close"],
     nextBtnText: "Volgende",
     prevBtnText: "Vorige",
     doneBtnText: "Sluit",
+    allowClose: false,
   });
 
-  driverInstance.setSteps(steps(driverInstance));
+  driverInstance.setSteps(
+    steps(driverInstance).map((step) => ({
+      ...step,
+      popover: {
+        ...step.popover,
+        showButtons: ["next", "close"],
+      },
+    }))
+  );
+
   if (autoStart) {
     waitUntilVisible(steps(driverInstance)[0].element).then(() => {
       driverInstance.drive();
