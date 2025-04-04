8b81089e963c8ea11daf3db53eec8771edfc746c
velomovies <22169955+velomovies@users.noreply.github.com>
2025-04-04 16:25:55 +0200

    fix(LayerPopup): fix table heading alignment

--- src/components/LayerPopup.vue
+++ src/components/LayerPopup.vue
@@ -10,7 +10,7 @@
     <table class="layer-meta__table" v-test="'meta-table'">
       <tr v-for="(value, key) in noIdMetadata" :key="key">
         <template v-if="key === 'link'">
-          <th>GisLink</th>
+          <th class="layer-meta__link">GisLink</th>
           <td class="layer-meta__link">
             <div v-html="sanitizedValue(value)"></div>
             <button
@@ -20,14 +20,22 @@
               type="button"
             >
              <template v-if="!isCopied">
-                <span class="sr-only">Kopieer GisLink</span>
                 <svg class="icon" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 448 512">
-                  <path d="M208 0L332.1 0c12.7 0 24.9 5.1 33.9 14.1l67.9 67.9c9 9 14.1 21.2 14.1 33.9L448 336c0 26.5-21.5 48-48 48l-192 0c-26.5 0-48-21.5-48-48l0-288c0-26.5 21.5-48 48-48zM48 128l80 0 0 64-64 0 0 256 192 0 0-32 64 0 0 48c0 26.5-21.5 48-48 48L48 512c-26.5 0-48-21.5-48-48L0 176c0-26.5 21.5-48 48-48z"/>                </svg>
+                  <path
+                    fill="currentColor"
+                    d="M208 0L332.1 0c12.7 0 24.9 5.1 33.9 14.1l67.9 67.9c9 9 14.1 21.2 14.1 33.9L448 336c0 26.5-21.5 48-48 48l-192 0c-26.5 0-48-21.5-48-48l0-288c0-26.5 21.5-48 48-48zM48 128l80 0 0 64-64 0 0 256 192 0 0-32 64 0 0 48c0 26.5-21.5 48-48 48L48 512c-26.5 0-48-21.5-48-48L0 176c0-26.5 21.5-48 48-48z"
+                  />
+                </svg>
+                <span class="sr-only">Kopieer GisLink</span>
              </template>
 
               <template v-else>
                 <svg class="icon" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 448 512">
-                  <path d="M438.6 105.4c12.5 12.5 12.5 32.8 0 45.3l-256 256c-12.5 12.5-32.8 12.5-45.3 0l-128-128c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0L160 338.7 393.4 105.4c12.5-12.5 32.8-12.5 45.3 0z"/>                </svg>
+                  <path
+                    fill="currentColor"
+                    d="M438.6 105.4c12.5 12.5 12.5 32.8 0 45.3l-256 256c-12.5 12.5-32.8 12.5-45.3 0l-128-128c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0L160 338.7 393.4 105.4c12.5-12.5 32.8-12.5 45.3 0z"
+                  />
+                </svg>
                 <span class="sr-only">Gekopieerd</span>
               </template>
             </button>
@@ -125,7 +133,11 @@ export default {
     background: none;
   }
 
-  .layer-meta__link {
+  th.layer-meta__link {
+    vertical-align: middle;
+  }
+
+  td.layer-meta__link {
     display: flex;
     align-items: center;
     gap: .5rem;
@@ -136,7 +148,6 @@ export default {
   }
 
   .btn.layer-meta__copy [class*=icon] {
-    fill: currentColor;
     margin: 0;
     width: 20px;
     height: 20px;
